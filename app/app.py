79726f26f4041905306874526972ea37d2401a5c
Mansha-Khod <manshakhod@gmail.com>
2026-05-30 12:02:24 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,6 +2,18 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
+import sys
+import os
+
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
+import utils.pantry as pantry
 import utils.pantry as pantry
 
 # -----------------------------------
